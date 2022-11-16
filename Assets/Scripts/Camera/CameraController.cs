6fbae6bf8e411ae6560670fd8c789a7cc19348a2
mr232432 <mr232432@falmouth.ac.uk>
2022-11-16 14:29:00 +0000

    feat: Added first iteration of camera focussing
    Player can now click to focus and click on a non planet to defocus

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -23,9 +23,13 @@ namespace Mattordev.Utils
         [Header("Camera Movement Variables")]
         public float xMovementSensitivity = 100f;
         public float yMovementSensitivity = 100f;
+        public float amountOfSmoothing;
+        private Vector2 posVelocity; 
+        private bool focusing;       
 
         // Background Variables
         [Header("BG Variables")]
+        public bool scaleBackground = true;
         public GameObject background;
 
         // Start is called before the first frame update
@@ -40,6 +44,7 @@ namespace Mattordev.Utils
         {
             MoveCameraWithKeyboardInput();
             ZoomScale();
+            FocusOnObject();
         }
 
         /// <summary>
@@ -53,11 +58,13 @@ namespace Mattordev.Utils
             mainCamera.orthographicSize = currentScale;
 
             // Scale the background with the screensize
-            ScaleBackground();
+            if(scaleBackground)
+                ScaleBackground();
         }
 
         /// <summary>
         /// Scales the background based on the cameras size.
+        /// This needs some fixing, as there's some strange behaviour when fully zoomed
         /// </summary>
         private void ScaleBackground()
         {
@@ -65,6 +72,9 @@ namespace Mattordev.Utils
             background.transform.localScale = new Vector3(calculatedScale, calculatedScale, calculatedScale) ;
         }
 
+        /// <summary>
+        /// Allows the use of camera movement with keyboard input.
+        /// </summary>
         public void MoveCameraWithKeyboardInput()
         {
             float x = Input.GetAxis("Horizontal") * Time.deltaTime * xMovementSensitivity;
@@ -73,6 +83,33 @@ namespace Mattordev.Utils
             Vector2 move = new Vector2(x, y);
 
             mainCamera.transform.position += new Vector3(move.x, move.y);
+        }
+
+        private void FocusOnObject()
+        {
+            Vector3 mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+                // if(hit.transform.tag == "Planet")
+                // {
+                //Debug.Log("Player is focusing on " + hit.transform.name);
+                MoveToClickedTarget(hit.transform);
+                    
+                // }
+            }
+        }
+
+        private void MoveToClickedTarget(Transform target)
+        {
+            if (target == null) {
+                transform.parent = null;
+                return;
+            }
+            //Vector2 newCameraPos = Vector2.SmoothDamp(transform.position, pos, ref posVelocity, amountOfSmoothing);
+            transform.position = new Vector3(target.position.x, target.position.y, -10);
+            transform.parent = target;
         }
     }
 }
