af7fb13fe06fe2814685982a2eeb29dd7713ddc3
mr232432 <mr232432@falmouth.ac.uk>
2022-11-24 13:50:24 +0000

    feat: added animations to pause menu (needs fix)

--- Assets/Scripts/UI/PauseMenuController.cs
+++ Assets/Scripts/UI/PauseMenuController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// Authored & Written by @mattordev
@@ -13,6 +14,13 @@ namespace Mattordev
     {
         public GameObject pMenu; // ref to the pause menu
         Canvas pMenuCanvas;
+        public GameObject settingsMenu;
+
+        // Internal
+        int pTimeScale = 0;
+        float unpausedTimeScale;
+
+        public Animator animator;
         // Start is called before the first frame update
         void Start()
         {
@@ -23,7 +31,7 @@ namespace Mattordev
         // Update is called once per frame
         void Update()
         {
-            
+            GetPauseInput();
         }
 
         /// <summary>
@@ -32,9 +40,53 @@ namespace Mattordev
         void Init()
         {
             pMenu = this.gameObject;
+            animator = GetComponent<Animator>();
             // Disable the pause menu at game start.
             pMenuCanvas = pMenu.GetComponent<Canvas>();
             pMenuCanvas.enabled = false;
+            // Get the unpaused timescale
+            unpausedTimeScale = Time.timeScale;
+        }
+
+        /// <summary>
+        /// Pauses the game by setting the timescale
+        /// </summary>
+        public void Pause(bool paused)
+        {
+            if (!paused)
+            {
+                Time.timeScale = unpausedTimeScale;
+                animator.SetTrigger("fadeOutPauseMenu");
+                return;
+            }
+            Time.timeScale = pTimeScale;
+            animator.SetTrigger("fadeInPauseMenu");
+        }
+
+        void GetPauseInput()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                // Pause the game
+                Pause(!pMenuCanvas.enabled);
+                pMenuCanvas.enabled = !pMenuCanvas.enabled;
+            }
+        }
+
+        public void QuitToMainMenu()
+        {
+            Time.timeScale = unpausedTimeScale;
+            SceneManager.LoadScene(0);
+        }
+
+        public void Settings()
+        {
+            // Play animation for opening settings
+            if (!settingsMenu.activeInHierarchy)
+            {
+                //animator.SetTrigger("fadeInSettings");
+            }
+            //animator.SetTrigger("fadeOutPauseMenu");
         }
     }
 }
