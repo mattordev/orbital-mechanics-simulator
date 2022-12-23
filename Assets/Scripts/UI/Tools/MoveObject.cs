a2f5a2bc4af2f2d035d290ae1764dfe861d43ead
mr232432 <mr232432@falmouth.ac.uk>
2022-12-23 14:46:36 +0000

    fix: narrowed down the raycast issue to the fact that I was pausing simulation on the rigidbodies

--- Assets/Scripts/UI/Tools/MoveObject.cs
+++ Assets/Scripts/UI/Tools/MoveObject.cs
@@ -20,8 +20,14 @@ namespace Mattordev.Utils
         public bool moving = false;
         public GameObject selectedObject;
 
+        public Camera mainCam;
         public CameraController cameraController;
         public StatisticsTracker statisticsTracker;
+
+        private void Start()
+        {
+            mainCam = Camera.main;
+        }
 
         // Update is called once per frame
         void Update()
@@ -32,45 +38,37 @@ namespace Mattordev.Utils
             }
         }
 
-        void MoveObjectToMouse()
-        {
-            if (Input.GetButtonDown("Fire1"))
-            {
-                selectedObject = cameraController.GetClickedItem();
-                Debug.Log(selectedObject);
-            }
-
-            // Make sure that the selected object isn't nothing, also allow the player to cancel
-            if (!selectedObject || Input.GetButtonDown("Fire2"))
-            {
-                // set moving to false to drop the planet at the current mouse pos
-                moving = false;
-                SimState(true);
-                return;
-            }
-
-            selectedObject.transform.position = cameraController.GetMousePos();
-
-            // Player clicks to drop the object
-            if (Input.GetButtonDown("Fire2"))
-            {
-                // set moving to false to drop the planet at the current mouse pos
-                moving = false;
-                SimState(true);
-
-                // Rigidbody2D rb2D = selectedObject.GetComponent<Rigidbody2D>();
-                // rb2D.simulated = false;
-            }
-
-        }
-
         public void StartObjectMove()
         {
             moving = true;
+            SimState(false);
+        }
+
+        void MoveObjectToMouse()
+        {
+            RaycastHit2D hit = Physics2D.Raycast(mainCam.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (Input.GetMouseButtonDown(0) && hit.transform != null)
+            {
+                selectedObject = hit.transform.gameObject;
+
+                Debug.Log(selectedObject);
+                SimState(true);
+                selectedObject.transform.position = cameraController.GetMousePos();
+            }
+
+            // Player clicks to drop the object
+            if (Input.GetMouseButtonDown(1))
+            {
+                // set moving to false to drop the planet at the current mouse pos
+                moving = false;
+                SimState(true);
+            }
         }
 
         /// <summary>
         /// Used for toggling the simulation without effecting Time.timeScale.
+        /// 
+        /// TODO: FIGURE OUT HOW TO FREEZE RIGIBODIES WHILST THEY STAY SIMULATED OTHERWISE RAYCASTS DONT WORK
         /// </summary>
         /// <param name="state"></param>
         public void SimState(bool state)
@@ -81,12 +79,32 @@ namespace Mattordev.Utils
             BodySimulation bodySimulation = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<BodySimulation>();
             bodySimulation.enabled = state;
 
+            if (state == true)
+            {
+                UnfreezePosition();
+            }
+            else
+            {
+                FreezePosition();
+            }
+
             // Stop simulation on the bodies
             foreach (Attractor attractor in statisticsTracker.attractors)
             {
-                Rigidbody2D rb2D = attractor.GetComponent<Rigidbody2D>();
-                rb2D.simulated = state;
+                attractor.enabled = state;
             }
+        }
+
+        // Freeze the position of the game object
+        void FreezePosition()
+        {
+
+        }
+
+        // Reapply the saved velocity
+        void UnfreezePosition()
+        {
+
         }
     }
 }
