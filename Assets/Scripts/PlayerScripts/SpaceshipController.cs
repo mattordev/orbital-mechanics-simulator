5072a04caede5f398c668072b79701146cdbf094
Matthew Roberts <mr232432@falmouth.ac.uk>
2023-10-03 02:38:13 +0100

    feat: basic ship movement
    controls: T - forward, F - left, H - right. Controls will change once I fix the camera rotation issues

--- Assets/Scripts/PlayerScripts/SpaceshipController.cs
+++ Assets/Scripts/PlayerScripts/SpaceshipController.cs
@@ -15,26 +15,20 @@ namespace Mattordev.Spaceship
     public class SpaceshipController : MonoBehaviour
     {
         [Header("Force Variables")]
-        public float rotationSpeed = 10f;
-        public float thrusterForce = 5f;
+        public float rotationSpeed = 100f;
+        public float thrusterForce = 1000f;
 
         [Header("Ship Options")]
         public bool inertialDampers = false;
-        public float intertialDamperForce;
+        public float intertialDamperForce = 100f;
 
         [Header("Other")]
-        public Sprite spaceShipSprite;
+        public SpriteRenderer spaceShipSprite;
         public Rigidbody2D rb2D;
         public KeyCode thrustForward = KeyCode.Space;
         public KeyCode rotateLeft = KeyCode.Q;
         public KeyCode rotateRight = KeyCode.E;
-
-
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
+        private bool isColliding = false;
 
         // Update is called once per frame
         void Update()
@@ -42,23 +36,64 @@ namespace Mattordev.Spaceship
             GetInput();
         }
 
+        /// <summary>
+        /// Gets the input for thrust and rotation
+        /// </summary>
         private void GetInput()
         {
-            if (Input.GetKeyDown(thrustForward))
+            if (Input.GetKey(thrustForward))
             {
-                Vector2 newForce = new Vector2();
-                rb2D.AddForce(newForce);
+                ApplyThrust(thrusterForce);
+            }
+            else
+            {
+                // Apply inertial dampeners here when no thrust input
+                ApplyInertialDampeners(intertialDamperForce);
             }
 
-            if (Input.GetKeyDown(rotateLeft))
+            if (Input.GetKey(rotateLeft))
             {
-
+                RotateObject(rotationSpeed);
             }
 
-            if (Input.GetKeyDown(rotateRight))
+            if (Input.GetKey(rotateRight))
             {
-
+                RotateObject(-rotationSpeed);
             }
+        }
+
+        // Function to smoothly rotate the object
+        // Function to smoothly rotate the object
+        private void RotateObject(float rotationAmount)
+        {
+            // Apply torque to the Rigidbody2D for rotation
+            rb2D.AddTorque(rotationAmount * Time.fixedDeltaTime, ForceMode2D.Force);
+        }
+
+        private void ApplyThrust(float thrusterForce)
+        {
+            float desiredAcceleration = thrusterForce / rb2D.mass;
+            // Calculate the force needed based on mass and desired acceleration
+            float force = rb2D.mass * desiredAcceleration;
+
+            // Apply the force in the forward direction of the object
+            rb2D.AddForce(transform.up * force, ForceMode2D.Force);
+        }
+
+        private void ApplyInertialDampeners(float dampeningFactor)
+        {
+            // Calculate the dampening force based on the velocity
+            Vector2 velocity = rb2D.velocity;
+            Vector2 dampeningForce = -velocity * dampeningFactor;
+
+            // Calculate the angular velocity
+            float angularVelocity = rb2D.angularVelocity;
+
+            // Apply the dampening force to linear motion
+            rb2D.AddForce(dampeningForce, ForceMode2D.Force);
+
+            // Apply torque in the opposite direction to stop rotation
+            rb2D.AddTorque(-angularVelocity * dampeningFactor, ForceMode2D.Force);
         }
     }
 }
