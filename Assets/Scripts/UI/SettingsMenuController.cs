4178f92d62513dbb954ee5ce161358c66d9661ad
Matthew <mrobertscgd@gmail.com>
2024-03-18 13:53:48 +0000

    feat: added script and Random Fact of the Day

---
+++ Assets/Scripts/UI/SettingsMenuController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <author>
+/// Authored & Written by @mattordev
+/// 
+/// for external use, please contact the author directly
+/// </author>
+namespace Mattordev.UI
+{
+    public class SettingsMenuController : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
