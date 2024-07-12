dd5654a2da4a66ee16790abb5a7441d39b7c586d
Matthew <mrobertscgd@gmail.com>
2024-07-12 21:43:13 +0100

    feat: added shop content changer and updated build ver

---
+++ Assets/Scripts/UI/ContentIndexer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <author>
+/// Authored & Written by @mattordev
+/// 
+/// for external use, please contact the author directly
+/// </author>
+namespace Mattordev.UI
+{
+    public class ContentIndexer : MonoBehaviour
+    {
+        public int buttonIndex; // The index of the button that relates to the content window.
+    }
+}
